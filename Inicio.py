70ef6397027bac60ea5c731c1e67520941a2c65e
Jera09 <99239922+Jera09@users.noreply.github.com>
2025-05-18 16:25:05 -0600

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 def inicio():
 
@@ -7,3 +8,19 @@ def inicio():
     st.write("")
     st.write("#### Del menu selecciona el programa de maestría al que perteneces. Dentro encontraras una serie de instrucciones, las cuales dependerán del programa al que perteneces. ####")
     st.write("#### Por favor tomate tu tiempo para realizar la selección, en caso de tener alguna duda contacta a tu coordinador. ####")
+    datos = [
+        ["Fianzas Estratégicas y Análisis de Negocios", "Juan Manuel Pérez Cruz", "manuel.perezcr@anahuac.mx"],
+        ["Mercadotecnia y Comunicación Estratégica", "Juan Manuel Pérez Cruz", "manuel.perezcr@anahuac.mx"],
+        ["Dirección Estratégica del Talento Humano", "Juan Manuel Pérez Cruz", "manuel.perezcr@anahuac.mx"],
+        ["Administración y Gestión Publica", "Juan Manuel Pérez Cruz", "manuel.perezcr@anahuac.mx"],
+        ["Estratégia y  Dirección de Empresas", "Armando Uriel García Santana", "armandou.garciasa@anahuac.mx"],
+        ["Innovación, Emprendimiento y Dirección de Tecnología", "Armando Uriel García Santana", "armandou.garciasa@anahuac.mx"],
+        ["Derecho Empresarial y Gobierno Corporativo", "Armando Uriel García Santana", "armandou.garciasa@anahuac.mx"],
+        ["Dirección Estratégica de Instituciones de Salud", "Armando Uriel García Santana", "armandou.garciasa@anahuac.mx"],
+        ["Big Data e Inteligencia Artificial", "José de Jesús Ramos Beltrán", "jesus.ramosb@anahuac.mx"],
+        ["Ingeniería para la Dirección de Operaciones", "José de Jesús Ramos Beltrán", "jesus.ramosb@anahuac.mx"]
+    ]
+
+    df = pd.DataFrame(datos, columns=['PROGRAMA', 'COORDINADOR', 'CORREO'])
+
+    st.table(df)
