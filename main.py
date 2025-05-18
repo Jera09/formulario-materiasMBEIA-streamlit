9a21c90500e09ca68e24fa9f4714dd7648caa265
Jera09 <99239922+Jera09@users.noreply.github.com>
2025-05-18 16:29:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import streamlit as st
-#from tables import tables as t
-#import dataframes
-#from charts import charts
 from Inicio import inicio 
-from MBDEIA import BD_IA as BD
+from MBDEIA import BD_IA
 from MEDE import MEDE
 from MMyCE import mmyce
 from MFEAN import MFEAN
+from MDTH import MDTH
+from MIDO import MIDO
 
 # Configuración inicial
 st.set_page_config(
@@ -22,16 +21,20 @@ if "menu" not in st.session_state:
     st.session_state.menu = "Inicio"
 
 with st.sidebar:
-    st.session_state.menu = st.selectbox("Menu", ["Inicio","Big Data e Inteligencia Artificial", "Estratégia y Dirección de Empresas", "Mercadotecnia y Comunicación Estratégica", "Finanzas Estratégicas y Análisis de Negocios"])
+    st.session_state.menu = st.selectbox("Menu", ["Inicio","Big Data e Inteligencia Artificial", "Estratégia y Dirección de Empresas", "Mercadotecnia y Comunicación Estratégica", "Finanzas Estratégicas y Análisis de Negocios", "Dirección Estratégica del Talento Humano", "Ingeniería para la Dirección de Operaciones"])
 
 
 if st.session_state.menu == "Inicio":
     inicio()
 elif st.session_state.menu == "Big Data e Inteligencia Artificial":
-    BD()
+    BD_IA()
 elif st.session_state.menu == "Estratégia y Dirección de Empresas":
     MEDE()
 elif st.session_state.menu == "Mercadotecnia y Comunicación Estratégica":
     mmyce()
 elif st.session_state.menu == "Finanzas Estratégicas y Análisis de Negocios":
     MFEAN()
+elif st.session_state.menu == "Dirección Estratégica del Talento Humano":
+    MDTH()
+elif st.session_state.menu == "Ingeniería para la Dirección de Operaciones":
+    MIDO()
