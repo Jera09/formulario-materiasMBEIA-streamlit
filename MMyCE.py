9b9390d6abb9f7957ca6c846cec7b448dd31f5d2
Jera09 <99239922+Jera09@users.noreply.github.com>
2025-07-01 16:42:04 -0600

    Update MMyCE.py

--- MMyCE.py
+++ MMyCE.py
@@ -326,7 +326,7 @@ def mmyce():
     
         # Agregar imagen de encabezado (centrada)
         try:
-            img = Image("encabezado.jpg", width=1.5*inch, height=1.5*inch)
+            img = Image("encabezado.jpg", width=1.5*inch, height=1.8*inch)
             img.hAlign = 'CENTER'  # Centrar la imagen
             elements.append(img)
         except:
