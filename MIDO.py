d99d47834e4b065078fcff4e2bbada6154cb515d
Jera09 <99239922+Jera09@users.noreply.github.com>
2025-07-01 16:41:25 -0600

    Update MIDO.py

--- MIDO.py
+++ MIDO.py
@@ -344,7 +344,7 @@ def MIDO():
     
         # Agregar imagen de encabezado (centrada)
         try:
-            img = Image("encabezado.jpg", width=1.5*inch, height=1.5*inch)
+            img = Image("encabezado.jpg", width=1.5*inch, height=1.8*inch)
             img.hAlign = 'CENTER'  # Centrar la imagen
             elements.append(img)
         except:
