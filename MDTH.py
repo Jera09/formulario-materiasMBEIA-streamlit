99027c029b2a0030a57d608516eb2af46f858cd9
Jera09 <99239922+Jera09@users.noreply.github.com>
2025-07-01 16:40:10 -0600

    Update MDTH.py

--- MDTH.py
+++ MDTH.py
@@ -314,7 +314,7 @@ def MDTH():
     
         # Agregar imagen de encabezado (centrada)
         try:
-            img = Image("encabezado.jpg", width=1.5*inch, height=1.5*inch)
+            img = Image("encabezado.jpg", width=1.5*inch, height=1.8*inch)
             img.hAlign = 'CENTER'  # Centrar la imagen
             elements.append(img)
         except:
