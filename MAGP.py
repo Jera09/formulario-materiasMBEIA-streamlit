6aeac5738f66ff65bdb4d3dc11be154b51a6c51f
Jera09 <99239922+Jera09@users.noreply.github.com>
2025-07-01 16:38:36 -0600

    Update MAGP.py

--- MAGP.py
+++ MAGP.py
@@ -335,7 +335,7 @@ def MAGP():
     
         # Agregar imagen de encabezado (centrada)
         try:
-            img = Image("encabezado.jpg", width=1.5*inch, height=1.5*inch)
+            img = Image("encabezado.jpg", width=1.5*inch, height=1.8*inch)
             img.hAlign = 'CENTER'  # Centrar la imagen
             elements.append(img)
         except:
