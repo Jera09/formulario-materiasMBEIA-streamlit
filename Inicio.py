4426699c727aa3cec980b991320fed73d33dd255
Jera09 <99239922+Jera09@users.noreply.github.com>
2026-01-06 11:05:11 -0600

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -12,10 +12,10 @@ def inicio():
         ["Finanzas Estratégicas y Análisis de Negocios", "Juan Manuel Pérez Cruz", "manuel.perezcr@anahuac.mx"],
         ["Mercadotecnia y Comunicación Estratégica", "Juan Manuel Pérez Cruz", "manuel.perezcr@anahuac.mx"],
         ["Dirección Estratégica del Talento Humano", "Juan Manuel Pérez Cruz", "manuel.perezcr@anahuac.mx"],
-        ["Administración y Gestión Publica", "Armando Uriel García Santana", "armandou.garciasa@anahuac.mx"],
+        ["Administración y Gestión Publica", "Karenm Ramírez Barbosa", "karenm.ramirezba@anahuac.mx"],
         ["Estratégia y  Dirección de Empresas", "Armando Uriel García Santana", "armandou.garciasa@anahuac.mx"],
         ["Innovación, Emprendimiento y Dirección de Tecnología", "Armando Uriel García Santana", "armandou.garciasa@anahuac.mx"],
-        ["Derecho Empresarial y Gobierno Corporativo", "Armando Uriel García Santana", "armandou.garciasa@anahuac.mx"],
+        ["Derecho Empresarial y Gobierno Corporativo", "Karenm Ramírez Barbosa", "karenm.ramirezba@anahuac.mx"],
         ["Dirección Estratégica de Instituciones de Salud", "Armando Uriel García Santana", "armandou.garciasa@anahuac.mx"],
         ["Big Data e Inteligencia Artificial", "José de Jesús Ramos Beltrán", "jesus.ramosb@anahuac.mx"],
         ["Ingeniería para la Dirección de Operaciones", "José de Jesús Ramos Beltrán", "jesus.ramosb@anahuac.mx"]
@@ -24,3 +24,4 @@ def inicio():
     df = pd.DataFrame(datos, columns=['PROGRAMA', 'COORDINADOR', 'CORREO'])
 
     st.table(df)
+
