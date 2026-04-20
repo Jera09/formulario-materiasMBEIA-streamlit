f35630d0e72176fe05ff2fa90dca18f7817e73df
Jera09 <99239922+Jera09@users.noreply.github.com>
2026-04-20 10:42:31 -0600

    Update MFEAN.py

--- MFEAN.py
+++ MFEAN.py
@@ -98,7 +98,7 @@ def MFEAN():
         "Ingeniería y Operaciones": [
             {"materia": "Sistemas de gestión empresarial*", "curso": "IIND", "clave": "55703", "créditos": 6},
             {"materia": "Six sigma*", "curso": "IIND", "clave": "55705", "créditos": 6},
-            {"materia": "Dirección de operaciones en Empresas", "curso": "IIND", "clave": "55700", "créditos": 7},
+            {"materia": "Dirección de operaciones en Empresas", "curso": "IIND", "clave": "55706", "créditos": 7},
             {"materia": "Logística y cadena de suministros", "curso": "IIND", "clave": "55702", "créditos": 6},
             {"materia": "Sistemas flexibles de Manufactura", "curso": "IIND", "clave": "55704", "créditos": 6},
         ],
